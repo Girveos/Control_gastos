a2d51720f2c136310e02ecf38ef96856a68066c2
Julián Alejandro Ledesma Zapata <ledesmazja@gmail.com>
2023-05-14 14:23:54 -0500

    Merge

--- src/models/charge.py
+++ src/models/charge.py
@@ -4,7 +4,7 @@ from src.database import db , ma
 class Charge (db.Model):
     id          = db.Column(db.Integer, primary_key=True, autoincrement=True)
     value       = db.Column(db.Float, nullable = False)
-    date        = db.Column(db.Date, nullable=True)
+    date        = db.Column(db.DateTime, nullable=True)
     description = db.Column(db.String(80), nullable=True)
     create_at   = db.Column(db.DateTime, default = datetime.now())
     update_at   = db.Column(db.DateTime, onupdate = datetime.now())
